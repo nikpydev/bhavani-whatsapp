3bc81115d339cea878dc2b528125276235445aa6
Nikhil Choudhary <nc1336282@gmail.com>
2020-09-08 22:32:57 +0530

    first commit

--- src/Containers/SidebarChat.js
+++ src/Containers/SidebarChat.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./SidebarChat.css";
+// import "./SidebarChat.scss";
 import { Avatar } from "@material-ui/core";
 
 function SidebarChat() {
   return (
     <div classname="sidebarChat">
-      <Avatar />
-      <div className="sidebarChat_info">
+      <div className="sidebarChat__avatar">
+        <Avatar />
+      </div>
+      <div className="sidebarChat__info">
         <h2>Room name</h2>
         <p>this is last message</p>
       </div>
